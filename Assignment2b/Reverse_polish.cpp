9d94866d080628484fa30776edc9709affb23145
MaksimLazarevSergeevich <st128707@student.spbu.ru>
2024-10-09 18:38:41 +0300

    Now you can use numbers with a dot, and the inscriptions have been changed to English

--- Assignment2b/Reverse_polish.cpp
+++ Assignment2b/Reverse_polish.cpp
@@ -5,9 +5,9 @@ second tusk b*/
 #include <string>
 #include <sstream>
 
-int calculate(int a, int b, std::string opers);
+double calculate(double a, double b, std::string opers);
 
-int calculate(int a, int b, std::string opers) {
+double calculate(double a, double b, std::string opers) {
   if (opers == "*") {
     return a * b;
   }else if(opers == "/") {
@@ -21,26 +21,26 @@ int calculate(int a, int b, std::string opers) {
 
 
 int main() {
-  std::cout << "Введите выражение в виде обратной польской записи через пробелы:" << '\n';
+  std::cout << "Enter the expression as reverse polish notation, separated by spaces:" << '\n';
 
   std::string input;
   std::string input_unstream;
   getline(std::cin, input_unstream);
   std::stringstream ss(input_unstream);
 
-  int* arr = new int [50];
-  int* q = &arr[0] - 1;
+  double* arr = new double [50];
+  double* q = &arr[0] - 1;
 
   while (getline(ss, input, ' ')) {
     if (input == "+" || input == "-" || input == "*" || input == "/") {
-      int result = calculate(*(q - 1), *q, input);
+      double result = calculate(*(q - 1), *q, input);
       q -= 1;
       *q = result;
     }else {
       q += 1;
-      *q = std::stoi(input);
+      *q = std::stod(input);
     }
   }
-  std::cout << "Ответ: " << *q << '\n';
+  std::cout << "Answer: " << *q << '\n';
   delete [] arr;
 }
